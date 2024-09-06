f7ca516c25fd7946bb3616430e5ebc44a1a9a14b
Ayiera-20 <ayieramyra@gmail.com>
2024-09-06 21:14:30 +0300

    Intro to python -user/input

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input ('Tell me your name')
+age = input ('Tell me your age')
+location = input ('Where do you live?')
+print(f'Hello{name}, you are{age} years old and you live in{location}')
